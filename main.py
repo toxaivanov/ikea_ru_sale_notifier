405e7440cbc60f631dc7ce73539fc8deecc61a68
toxaivanov <toxaivanov@me.com>
2022-07-07 01:46:21 +0300

    main script

---
+++ main.py
@@ -0,0 +1,60 @@
+"""main file of project"""
+import json
+import os
+from time import sleep
+
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+CHROMEDRIVER_PATH = "/usr/local/bin/chromedriver"
+DATADIR = "~/chrdrv"
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"  # noqa pylint: disable=line-too-long
+SRC_PAGE = "https://www.ikea.com/ru/ru/customer-service/contact-us/zayavka-na-oformlenie-zakaza-pub1a73c1b0"  # noqa pylint: disable=line-too-long
+IFTTT_WEBHOOK = os.getenv("POETRY_IFTTT_WEBHOOK")
+
+
+def send_notification():
+    """triggers IFTTT webhook"""
+
+    payload = json.dumps(
+        {
+            "value1": "<b>Форма для заказов снова доступна на сайте</b><br/>",
+            "value2": SRC_PAGE,
+        }
+    )
+    headers = {"Content-Type": "application/json"}
+
+    print("wh", IFTTT_WEBHOOK)
+
+    requests.request("POST", IFTTT_WEBHOOK, headers=headers, data=payload)
+
+
+def main():
+    """main function of script"""
+    ser = Service(CHROMEDRIVER_PATH)
+
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument(f"user-data-dir={DATADIR}")
+    options.add_argument(f"user-agent={USER_AGENT}")
+
+    driver = webdriver.Chrome(service=ser, options=options)
+
+    while True:
+
+        driver.get(SRC_PAGE)
+
+        src = driver.page_source
+        if "Прием новых заявок запустится немного позже" in src:
+            send_notification()
+            break
+
+        sleep(12)
+
+    driver.quit()
+
+
+if __name__ == "__main__":
+    main()
